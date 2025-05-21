cf41c0f052c342bd766639e226ac4256409e8c10
Renato <renato.vergara.dev@gmail.com>
2025-05-21 20:17:59 +0200

    feat(Footer): enhance component with styling and spacing adjustments

--- client2/src/layout/Footer.jsx
+++ client2/src/layout/Footer.jsx
@@ -1,14 +1,14 @@
 import { FaGithub } from "react-icons/fa";
 export default function Footer() {
     return (
-        <footer className="flex items-center justify-center">
+        <footer className="flex items-center justify-center py-4 gap-4">
             <a
                 href="https://github.com/arcvra/idea-harvest"
                 target="blank"
             >
                 <FaGithub />
             </a>
-            <p>Made by @arcvra with ❤️</p>
+            <p className="text-xs">Made by @arcvra with ❤️</p>
         </footer>
     )
 }
