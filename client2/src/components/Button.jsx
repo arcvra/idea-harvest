03d7cf94d47a4bda745b54b832ef74029bdc29a8
Ren <renato.vergara.dev@gmail.com>
2025-05-24 15:20:02 +0200

    feat(Button): add default props and support for children content
    - Set default values for `type` prop
    - Add base style through  default `className`
    - Replace `text` prop with `children` for flexible button content
    - Add JSDoc-style comments for prop documentation

--- client2/src/components/Button.jsx
+++ client2/src/components/Button.jsx
@@ -1,11 +1,30 @@
-export const Button = ({ type = "button", onClick, name }) => {
+/**
+ * 
+ * @param {string} [type="button"] - HTML button type
+ * @param {string} [name] - Button's name (used for forms)
+ * @param {string} [id] - Button's ID (used for labels)
+ * @param {string} [className] - Tailwind or custom classes
+ * @param {function} [onClick] - Button click handler
+ * @param {React.ReactNode} children - Button inner content
+ * @returns {JSX.Element}
+ */
+
+export const Button = ({
+    type = "button",
+    name,
+    id,
+    className = "",
+    onClick,
+    children }) => {
     return (
         <button
             type={type}
             onClick={onClick}
-            className="px-2 py-1 cursor-pointer border-2 border-violet-900 rounded-xl"
+            name={name}
+            id={id}
+            className={`px-2 py-1 cursor-pointer border-2 border-violet-900 rounded-xl ${className}`}
         >
-            {name}
-        </button>
+            {children}
+        </button >
     )
 }
