1e03280f675fb66d211ac50df13e6a6733fc6907
Ren <renato.vergara.dev@gmail.com>
2025-05-24 17:14:51 +0200

    feat(PostForm): enhance form layout and correct required prop usage for inputs

--- client2/layout/PostForm.jsx
+++ client2/layout/PostForm.jsx
@@ -14,25 +14,30 @@ export const PostForm = () => {
     } = usePostForm();
 
     return (
-        <form onSubmit={handleSubmit} >
+        <form onSubmit={handleSubmit} className="grid px-2 py-5 border border-violet-950 rounded-2xl shadow shadow-neutral-400">
             <Input
                 placeholder="What are you thinking about?"
                 value={conceptValue}
                 name="conceptInput"
                 id="conceptInput"
-                required="true"
-                onChange={handleConceptInput} />
+                required={true}
+                onChange={handleConceptInput} 
+                className="m-3"
+                />
             <Input
                 placeholder="Want to elaborate?"
                 value={descriptionValue}
                 name="descriptionInput"
                 id="descriptionInput"
-                onChange={handleDescriptionInput} />
+                onChange={handleDescriptionInput} 
+                className="m-3"
+                />
             <Button
                 type="submit"
                 name="postIdea"
                 id="postIdea"
                 children="Post idea"
+                className="justify-self-center"
             />
         </form>
     )
