93e330465250f90237e7d52385537d409e34e414
Ren <renato.vergara.dev@gmail.com>
2025-05-24 14:17:38 +0200

    feat(Input): expand Input component to include name, id, className, autoComplete, and required props

--- client2/src/components/Input.jsx
+++ client2/src/components/Input.jsx
@@ -1,9 +1,24 @@
-export const Input = ({ type = "text", placeholder, value, onChange }) => {
+export const Input = ({
+    type = "text",
+    placeholder,
+    value,
+    name,
+    id,
+    className = "",
+    autoComplete,
+    required = false,
+    onChange
+}) => {
     return (
         <input
             type={type}
             value={value}
             placeholder={placeholder}
+            className={`${className}`}
+            name={name}
+            id={id}
+            autoComplete={autoComplete}
+            required={required}
             onChange={onChange}
         />
     )
