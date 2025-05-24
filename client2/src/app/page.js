ed4615681cc37b1ab7429bdc8ef5b98eb831097c
Ren <renato.vergara.dev@gmail.com>
2025-05-24 17:25:38 +0200

    fix(Home): wrap PostForm component in a div for proper layout structure

--- client2/src/app/page.js
+++ client2/src/app/page.js
@@ -2,8 +2,8 @@ import { PostForm } from "../../layout/PostForm";
 
 export default function Home() {
   return (
-    <>
+    <div className="self-center justify-self-center">
       <PostForm />
-    </>
+    </div>
   );
 }
